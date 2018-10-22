304d22d87a6b6cfc1e21b4117f4dee4f8d5f8b9d
Aleksandrov91 <a.aleksandrov91@gmail.com>
2018-10-22 23:37:06 +0300

    Create base view render.

---
+++ SIS.MvcFramework/ActionResults/ViewResult.cs
@@ -0,0 +1,19 @@
+﻿namespace SIS.MvcFramework.ActionResults
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using SIS.MvcFramework.ActionResults.Contracts;
+
+    public class ViewResult : IViewable
+    {
+        public ViewResult(IRendable view)
+        {
+            this.View = view;
+        }
+
+        public IRendable View { get; set; }
+
+        public string Invoke() => this.View.Render();
+    }
+}
---
+++ SIS.MvcFramework/Views/View.cs
@@ -0,0 +1,27 @@
+﻿namespace SIS.MvcFramework.Views
+{
+    using System.IO;
+    using SIS.MvcFramework.ActionResults.Contracts;
+
+    public class View : IRendable
+    {
+        private readonly string fullyQualifiedTemplateName;
+
+        public View(string fullyQualifiedTemplateName)
+        {
+            this.fullyQualifiedTemplateName = fullyQualifiedTemplateName;
+        }
+
+        public string Render() => this.ReadFile();
+
+        private string ReadFile()
+        {
+            if (!File.Exists(this.fullyQualifiedTemplateName))
+            {
+                throw new FileNotFoundException($"View does not exists at {this.fullyQualifiedTemplateName}");
+            }
+
+            return File.ReadAllText(this.fullyQualifiedTemplateName);
+        }
+    }
+}
