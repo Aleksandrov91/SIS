b54c1949bd3f59d6365fc5971e67f0e3112e1434
Aleksandrov91 <a.aleksandrov91@gmail.com>
2018-10-26 21:21:43 +0300

    Added number validation

---
+++ SIS.MvcFramework/Attributes/Properties/NumberRangeAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace SIS.MvcFramework.Attributes.Properties
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class NumberRangeAttribute : ValidationAttribute
+    {
+        private readonly double minValue;
+        private readonly double maxValue;
+
+        public NumberRangeAttribute(double minValue, double maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            double valueAsNumber = (double)value;
+
+            return this.minValue <= valueAsNumber && this.maxValue >= valueAsNumber;
+        }
+    }
+}
